29e4596d4bf2541c8a7ae861ec205ab1194500d1
Naveen Belson <107055211+belson7044@users.noreply.github.com>
2024-08-14 17:44:40 +0530

    Add files via upload

---
+++ Portfolio/script.js
@@ -0,0 +1,9 @@
+window.addEventListener('scroll', function() {
+    const navbar = document.querySelector('.navbar');
+    if (window.scrollY > 50) { // Adjust scroll threshold as needed
+        navbar.classList.add('scrolled');
+    } else {
+        navbar.classList.remove('scrolled');
+    }
+});
+   
